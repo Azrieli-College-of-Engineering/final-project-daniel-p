b35462e9930c94afbcd9060ee39715757c4b4dc3
devilking246 <104373848+devilking246@users.noreply.github.com>
2026-02-28 10:08:16 +0200

    end

---
+++ evil.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/collect')
+def collect():
+    # קבלת המידע המשולב מהפרמטר 'info'
+    info = request.args.get('info')
+    if info:
+        print("\n" + "="*30)
+        print("[!]new data:")
+        print(info)
+        print("="*30 + "\n")
+    return "Received", 200
+
+if __name__ == '__main__':
+    # הרצה בפורט 5000
+    app.run(host='0.0.0.0', port=5000)
---
+++ main.js
@@ -0,0 +1,49 @@
+const expr = require ('express');
+const app = expr();
+const path = require('path');
+
+app.use(expr.urlencoded({ extended: true}));
+// 1. 
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+        <head><title>מערכת רישום מאובטחת</title></head>
+        <body style="font-family: sans-serif; direction: rtl; text-align: center;">
+            <h2>אנא הירשם למערכת</h2>
+            <form action="/confirm" method="POST" style="display: inline-block; text-align: right;">
+                מייל: <br><input type="email" name="email" id="email" required><br><br>
+                שם משתמש: <br><input type="text" name="user" id="user" required><br><br>
+                סיסמה: <br><input type="password" name="pass" id="pass" required><br><br>
+                מיקוד: <br><input type="text" name="zip" id="zip" required><br><br>
+                <button type="submit">שלח נתונים</button>
+            </form>
+        </body>
+        </html>
+    `);
+});
+let user_db =[]; // a db for all user
+
+// 2
+app.post('/confirm', (req, res) => {
+    const { email, user, zip ,pass} = req.body;
+    user_db.push({user, email, pass, zip});
+    let userList = user_db.map(u => {u.user}).join(' | ');
+    res.send(`
+       <html>
+        <body style="direction: rtl; text-align: center;">
+            <h1>thanks:, ${user}!</h1>
+            <p>we sent a email to : ${email}</p>
+            <p>your zip:${zip}</p>
+            <p>password:${pass}</p>
+            <a href="/">חזור לדף הבית</a>
+            <br><br><br>
+            <hr><h3> want to know your friends?</h3>
+            <div>${userList}</div> 
+        </body>
+        </html>
+
+    `);
+});
+
+app.listen(3000, () => {
+    console.log('server on : http://localhost:3000')});
